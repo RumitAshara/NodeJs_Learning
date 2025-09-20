e9bacafb2542d71bf31f26494fc3eaae603d0c30
RumitAshara <126247287+RumitAshara@users.noreply.github.com>
2025-09-20 14:10:00 +0530

    Hospital and Ecommerce Management Schema

---
+++ Models/hospital-management/medical_record.models.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const medicalRecordSchema = new mongoose.Schema({
+
+}
+,{
+        timestamps:true,
+})
+
+export const MedicalRecord = mongoose.model("MedicalRecord",medicalRecordSchema);
